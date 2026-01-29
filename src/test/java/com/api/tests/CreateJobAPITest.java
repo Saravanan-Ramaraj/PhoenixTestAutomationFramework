ffbdda6951b40d771fb83e815136a75a62ebe8d8
Saravanan Ramaraj <saravananoffcl@gmail.com>
2026-01-29 10:22:44 +0530

    DateTimeUtil class
    Created DateTimeUtil class to provide us with a past ISO standard UTC
    Time

--- src/test/java/com/api/tests/CreateJobAPITest.java
+++ src/test/java/com/api/tests/CreateJobAPITest.java
@@ -15,6 +15,7 @@ import com.api.request.model.Customer;
 import com.api.request.model.CustomerAddress;
 import com.api.request.model.CustomerProduct;
 import com.api.request.model.Problems;
+import static com.api.utils.DateTimeUtil.*;
 import com.api.utils.SpecUtil;
 
 import static io.restassured.module.jsv.JsonSchemaValidator.*;
@@ -29,7 +30,7 @@ public class CreateJobAPITest {
 		// Creating the CreateJobPayload
 		Customer customer = new Customer("Ram", "Kumar", "8562479620", "", "test@test.com", "");
 		CustomerAddress customerAddress = new CustomerAddress("D098", "Urbanrise", "West st", "Chettinad", "Kelambakkam", "600040", "India", "Tamil Nadu");
-		CustomerProduct customerProduct = new CustomerProduct("2025-10-21T18:30:00.000Z", "13510700420678", "13510700420678", "13510700420678", "025-10-21T18:30:00.000Z", 1, 1);
+		CustomerProduct customerProduct = new CustomerProduct(getTimeWithDaysAgo(10), "13511700420678", "13511700420678", "13511700420678", getTimeWithDaysAgo(10), 1, 1);
 		Problems problems = new Problems(1, "Battery Issue");
 		List<Problems> problemList = new ArrayList<Problems>();
 		problemList.add(problems);
