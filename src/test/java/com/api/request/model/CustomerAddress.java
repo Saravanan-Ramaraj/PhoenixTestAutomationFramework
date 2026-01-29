f0a42a5e18af44fadb1c2ddb9a759ad45a0f2a14
Saravanan Ramaraj <saravananoffcl@gmail.com>
2026-01-29 09:21:26 +0530

    Refactored from com.api.pojo to com.api.request.model

--- src/test/java/com/api/request/model/CustomerAddress.java
+++ src/test/java/com/api/request/model/CustomerAddress.java
@@ -2,7 +2,6 @@ package com.api.request.model;
 
 public record CustomerAddress(String flat_number, String apartment_name, String street_name, String landmark,
 		String area, String pincode, String country, String state)
-
 {
 
 }
