6900e2a144b37b6e84502a0d89804f8b6fa90055
Saravanan Ramaraj <saravananoffcl@gmail.com>
2026-02-19 10:55:01 +0530

    Completed The Requirement 11
    Created Excel Reader Utility that will read the excel file from the
    testData and will be used in the DataProvider Component.
    
    Tests have been added in the testng-datadriven.xml

--- src/test/java/com/api/utils/ExcelReaderUtil.java
+++ src/test/java/com/api/utils/ExcelReaderUtil.java
@@ -2,39 +2,37 @@ package com.api.utils;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Iterator;
+import java.util.List;
 
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import com.dataproviders.api.bean.UserBean;
+import com.poiji.bind.Poiji;
+
 public class ExcelReaderUtil {
 
-	public static void main(String[] args) throws IOException {
+	private ExcelReaderUtil() {
+
+	}
+
+	public static <T> Iterator<T> loadTestData(String xlsxFile, String sheetname, Class<T> clazz) {
 
 		InputStream is = Thread.currentThread().getContextClassLoader()
 				.getResourceAsStream("testData/PhoenixTestData.xlsx");
-		XSSFWorkbook myWorkbook = new XSSFWorkbook(is);
-		XSSFSheet mySheet = myWorkbook.getSheet("LoginTestData");
-
-		XSSFRow myRow;
-		XSSFCell myCell;
-
-		int lastRowIndex = mySheet.getLastRowNum();
-		System.out.println("Last row ind :" + lastRowIndex);
-
-		XSSFRow rowHeader = mySheet.getRow(0);
-		int lastIndexOfCol = rowHeader.getLastCellNum() - 1;
-		System.out.println("Last col ind :" + lastIndexOfCol);
-
-		for (int rowInd = 0; rowInd <= lastRowIndex; rowInd++) {
-			for (int colInd = 0; colInd <= lastIndexOfCol; colInd++) {
-				myRow = mySheet.getRow(rowInd);
-				myCell = myRow.getCell(colInd);
-				System.out.print(myCell + " ");
-			}
-			System.out.println(" ");
+		XSSFWorkbook myWorkbook = null;
+		try {
+			myWorkbook = new XSSFWorkbook(is);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+
+		XSSFSheet mySheet = myWorkbook.getSheet(sheetname);
+
+		List<T> dataList = Poiji.fromExcel(mySheet, clazz);
+
+		return dataList.iterator();
 
 	}
 
